84aa53123dbb9d879cbad1a79cdf8718ddeee929
Diego Borrero <die.9511@gmail.com>
2018-10-01 21:58:21 -0500

    Test Changed

--- src/main/test/eci.cosw/data/TodoRepositoryTest.java
+++ src/main/test/eci.cosw/data/TodoRepositoryTest.java
@@ -15,18 +15,26 @@ public class TodoRepositoryTest {
     @Autowired
     TodoRepository todoRepository;
 
-    @Test
-    public void findByResponsible() {
+    @Before
+    public void init() {
         todoRepository.deleteAll();
 
         todoRepository.save(new Todo("travel to Galapagos", 10, "Jan 10 - 1860", "charles@natural.com", "pending"));
         todoRepository.save(new Todo("travel to Galapagos", 10, "June 1 - 1870", "whatever@natural.com", "pending"));
         todoRepository.save(new Todo("travel to Galapagos", 10, "Feb 10 - 1890", "whatever2@natural.com", "pending"));
         todoRepository.save(new Todo("travel to wherever", 2, "Oct 10 - 1900", "charles@natural.com", "pending"));
-
-        for (Todo todo: todoRepository.findByResponsible("charles@natural.com")) {
-            Assert.assertTrue(todo.getResponsible().equals("charles@natural.com"));
-            System.out.println(todo);
-        }
     }
+
+    @Test
+    public void itShouldListTwo() {
+        List<Todo> todos = todoRepository.findResponsible("charles@natural.com");
+        assertEquals(2,todos.size());
+    }
+
+    @Test
+    public void itShouldListZero() {
+        List<Todo> todos = todoRepository.findResponsible("nobody@natural.com");
+        assertEquals(0,todos.size());
+    }
+
 }
